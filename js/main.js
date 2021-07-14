ee40dc5533c6b4f44c71404c630cbf4666070094
skyheat47295 <70936076+skyheat47295@users.noreply.github.com>
2021-07-14 03:08:19 -0700

    7.14.21

--- js/main.js
+++ js/main.js
@@ -20,3 +20,12 @@ function openModal() {
 function closeModal() {
     modal.style.display = 'none';
 }
+
+function getTextWidth(text, font) {
+    // re-use canvas object for better performance
+    var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
+    var context = canvas.getContext("2d");
+    context.font = font;
+    var metrics = context.measureText(text);
+    return metrics.width;
+}
